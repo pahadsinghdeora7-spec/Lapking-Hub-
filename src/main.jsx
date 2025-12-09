af12a1b7e4dcab333514d1c3aab50d8c131eb7c4
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2025-12-09 19:41:56 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
+// src/main.jsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
+// IMPORTANT: yahi se hum CSS file load kar rahe hain
+import './Styles.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* app-root class ko CSS me style kiya hai */}
+    <div className="app-root">
+      <App />
+    </div>
   </React.StrictMode>
 );
