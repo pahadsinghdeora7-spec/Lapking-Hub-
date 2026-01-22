ec2ac6be01f71ef6e89f4712a3a2ea3dfe1f1932
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-22 10:39:22 +0530

    Create dummyProducts.js

---
+++ src/data/dummyProducts.js
@@ -0,0 +1,25 @@
+const products = [
+  {
+    id: 1,
+    brand: "DELL",
+    name: "Dell DC Jack 65W",
+    price: 299,
+    image: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee"
+  },
+  {
+    id: 2,
+    brand: "LENOVO",
+    name: "Lenovo Laptop Keyboard",
+    price: 1250,
+    image: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8"
+  },
+  {
+    id: 3,
+    brand: "HP",
+    name: "HP Pavilion Keyboard",
+    price: 450,
+    image: "https://images.unsplash.com/photo-1518770660439-4636190af475"
+  }
+];
+
+export default products;
