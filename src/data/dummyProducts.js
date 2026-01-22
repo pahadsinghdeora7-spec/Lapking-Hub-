e7c0bd9c93e1a2244a10914777d5747391779182
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-22 13:37:44 +0530

    Update dummyProducts.js

--- src/data/dummyProducts.js
+++ src/data/dummyProducts.js
@@ -3,23 +3,56 @@ const products = [
     id: 1,
     brand: "DELL",
     name: "Dell DC Jack 65W",
+    slug: "dell-dc-jack-65w",
+    category: "dc-jack",
     price: 299,
-    image: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee"
+    image:
+      "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
   },
+
   {
     id: 2,
     brand: "LENOVO",
     name: "Lenovo Laptop Keyboard",
+    slug: "lenovo-laptop-keyboard",
+    category: "keyboards",
     price: 1250,
-    image: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8"
+    image:
+      "https://images.unsplash.com/photo-1517336714731-489689fd1ca8",
   },
+
   {
     id: 3,
     brand: "HP",
     name: "HP Pavilion Keyboard",
+    slug: "hp-pavilion-keyboard",
+    category: "keyboards",
     price: 450,
-    image: "https://images.unsplash.com/photo-1518770660439-4636190af475"
-  }
+    image:
+      "https://images.unsplash.com/photo-1518770660439-4636190af475",
+  },
+
+  {
+    id: 4,
+    brand: "DELL",
+    name: "Dell Laptop Battery",
+    slug: "dell-laptop-battery",
+    category: "batteries",
+    price: 1850,
+    image:
+      "https://images.unsplash.com/photo-1587825140708-dfaf72ae4b04",
+  },
+
+  {
+    id: 5,
+    brand: "HP",
+    name: "HP Laptop Fan",
+    slug: "hp-laptop-fan",
+    category: "fans",
+    price: 699,
+    image:
+      "https://images.unsplash.com/photo-1587202372775-e229f172b9d1",
+  },
 ];
 
 export default products;
