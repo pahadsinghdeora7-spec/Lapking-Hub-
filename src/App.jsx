78fdaa7c776e6ac23260482cb17877426c1ec4b6
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-22 19:13:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -53,10 +53,9 @@ export default function App() {
             <Route path="/checkout/shipping" element={<CheckoutShipping />} />
             <Route path="/checkout/address" element={<CheckoutAddress />} />
             <Route path="/order/success/:id" element={<OrderSuccess />} />
-            <Route
-  path="/replacement/:orderId/:productId"
-  element={<ReplacementRequest />}
-/>
+            <Route path="/replacement/:orderId/:productId" element={<ReplacementRequest />}/>
+            <Route path="/replacement/:orderId/:productId"  element={<ReplacementRequest />}/>
+            
             {/* ================= ADMIN ROUTES ================= */}
             <Route path="/admin/login" element={<AdminLogin />} />
 
