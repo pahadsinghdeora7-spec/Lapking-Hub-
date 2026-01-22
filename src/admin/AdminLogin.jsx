aa4abb82943dba2e5599ee263f4ee6bda40bda74
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-22 11:44:09 +0530

    Update AdminLogin.jsx

--- src/admin/AdminLogin.jsx
+++ src/admin/AdminLogin.jsx
@@ -1,1 +1,43 @@
+import { useState } from "react";
+import { supabase } from "../supabase";
+import { useNavigate } from "react-router-dom";
 
+export default function AdminLogin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const login = async () => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      alert(error.message);
+    } else {
+      navigate("/admin");
+    }
+  };
+
+  return (
+    <div style={{ maxWidth: 350, margin: "100px auto" }}>
+      <h2>Admin Login</h2>
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+
+      <button onClick={login}>Login</button>
+    </div>
+  );
+}
