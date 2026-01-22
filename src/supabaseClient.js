1df690d65e27c12dd3220370fa45f1d47b2afff0
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-22 10:08:47 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://oprsvuzvabyhpwtepoj.supabase.co";
-
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wcnN1enZiYWJ5aHBvd3RlcG9qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5OTAyNzksImV4cCI6MjA4NDU2NjI3OX0.vN18eKV3EOPIrNoiUqFCinYaleOHS6Fld9vCaDsBw2c";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(
   supabaseUrl,
