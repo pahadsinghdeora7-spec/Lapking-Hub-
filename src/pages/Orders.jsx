7422ddbba604ce377c97e4027b098157bf7aa74e
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-23 20:47:45 +0530

    Update Orders.jsx

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { supabase } from "../supabaseClient";
+import { supabase } from "../supabaseClient.js";
 import "./Orders.css";
 
 export default function Orders() {
