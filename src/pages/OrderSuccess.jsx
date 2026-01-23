bc5cbabb61c708b92b58905a2703c4162710828c
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-23 20:47:14 +0530

    Update OrderSuccess.jsx

--- src/pages/OrderSuccess.jsx
+++ src/pages/OrderSuccess.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
-import { supabase } from "../supabaseClient";
+import { supabase } from "../supabaseClient.js";
 import "./OrderSuccess.css";
 
 export default function OrderSuccess() {
