86c276b9baa91b5eb484e34bc45b6ebb5cb81bdb
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-23 07:49:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,31 @@ import {
 } from "react-router-dom";
 
 /* ================= USER PAGES ================= */
+
 import Home from "./pages/Home.jsx";
 import Categories from "./pages/Categories.jsx";
 import Cart from "./pages/Cart.jsx";
 import Orders from "./pages/Orders.jsx";
 import Account from "./pages/Account.jsx";
 import CategoryProducts from "./pages/CategoryProducts.jsx";
-import CheckoutShipping from "./pages/CheckoutShipping";
-import CheckoutAddress from "./pages/CheckoutAddress";
-import OrderSuccess from "./pages/OrderSuccess";
+import CheckoutShipping from "./pages/CheckoutShipping.jsx";
+import CheckoutAddress from "./pages/CheckoutAddress.jsx";
+import OrderSuccess from "./pages/OrderSuccess.jsx";
+import ReplacementRequest from "./pages/ReplacementRequest.jsx";
 
 /* ================= ADMIN PAGES ================= */
+
 import AdminLogin from "./admin/AdminLogin.jsx";
 import AdminLayout from "./admin/AdminLayout.jsx";
 import AdminDashboard from "./admin/AdminDashboard.jsx";
 import AdminProducts from "./admin/AdminProducts.jsx";
 import AdminCategories from "./admin/AdminCategories.jsx";
 import AdminOrders from "./admin/AdminOrders.jsx";
+import AdminOrderView from "./admin/AdminOrderView.jsx";
+import AdminReplacements from "./admin/AdminReplacements.jsx";
 
-/* ================= COMMON ================= */
+/* ================= COMMON COMPONENTS ================= */
+
 import Header from "./components/Header.jsx";
 import BottomNav from "./components/BottomNav.jsx";
 import WhatsAppButton from "./components/WhatsAppButton.jsx";
@@ -44,6 +50,7 @@ export default function App() {
           <Routes>
 
             {/* ================= USER ROUTES ================= */}
+
             <Route path="/" element={<Home />} />
             <Route path="/categories" element={<Categories />} />
             <Route path="/cart" element={<Cart />} />
@@ -53,10 +60,15 @@ export default function App() {
             <Route path="/checkout/shipping" element={<CheckoutShipping />} />
             <Route path="/checkout/address" element={<CheckoutAddress />} />
             <Route path="/order/success/:id" element={<OrderSuccess />} />
-            <Route path="/replacement/:orderId/:productId" element={<ReplacementRequest />}/>
-            
-            
+
+            {/* 🔁 REPLACEMENT REQUEST (Customer) */}
+            <Route
+              path="/replacement/:orderId/:productId"
+              element={<ReplacementRequest />}
+            />
+
             {/* ================= ADMIN ROUTES ================= */}
+
             <Route path="/admin/login" element={<AdminLogin />} />
 
             <Route path="/admin" element={<AdminLayout />}>
