2aca3e5b89b1966db5390467bd0eb63031d3e03c
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-23 09:47:45 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,29 @@
-import "./Home.css";
-import products from "../data/dummyProducts";
+import { useEffect, useState } from "react";
+import { supabase } from "../supabaseClient";
 import ProductCard from "../components/ProductCard";
+import "./Home.css";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  async function loadProducts() {
+    const { data, error } = await supabase
+      .from("products")
+      .select("*")
+      .eq("status", true)
+      .order("created_at", { ascending: false });
+
+    if (!error) {
+      setProducts(data);
+    }
+  }
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
   return (
     <div className="home">
-
-      {/* SEARCH */}
-      <div className="search-box">
-        <input placeholder="Search products..." />
-      </div>
 
       {/* BANNER */}
       <div className="banner">
@@ -17,23 +31,21 @@ export default function Home() {
         <p>
           Shop the best chargers, batteries, keyboards and more for all laptop brands
         </p>
-        <button>Shop Now →</button>
+        <button>Shop Now</button>
       </div>
 
-      {/* INFO */}
-      <div className="info-box">
-        Wholesale Supplier of Laptop Spare Parts & Accessories
-      </div>
-
-      {/* PRODUCTS */}
       <h3 className="section-title">Latest Products</h3>
 
       <div className="product-grid">
-        {products.map((item) => (
+        {products.length === 0 && (
+          <p style={{ padding: 20 }}>No products found</p>
+        )}
+
+        {products.map(item => (
           <ProductCard key={item.id} product={item} />
         ))}
       </div>
 
     </div>
   );
-}
+      }
