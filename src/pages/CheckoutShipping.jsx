fde1c864a9e4ed269fc5740fbac7bef08678de3f
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-23 20:58:57 +0530

    Update CheckoutShipping.jsx

--- src/pages/CheckoutShipping.jsx
+++ src/pages/CheckoutShipping.jsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { supabase } from "../supabaseClient.js";
+import { supabase } from "../supabaseClient";
 import "./CheckoutShipping.css";
 
 export default function CheckoutShipping() {
