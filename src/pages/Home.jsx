e5065a76e7efde603cc73b5e15c8cd4b2473b203
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-23 20:46:40 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "../supabaseClient";
+import { supabase } from "../supabaseClient.js";
 import ProductCard from "../components/ProductCard";
 import "./Home.css";
 
