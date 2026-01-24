f426a3f695df88dea2f953ce2242cdd474122c8f
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-24 11:43:47 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,22 @@
+import { MdFavoriteBorder, MdShoppingCart, MdMenu } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 import "./header.css";
 
 export default function Header() {
+  const navigate = useNavigate();
+
   return (
     <header className="header">
       <div className="header-left">
-        <span className="menu">☰</span>
-        <h1 className="logo">
-          👑 Lapking<span>Hub</span>
-        </h1>
+        <MdMenu size={26} />
+        <h2 onClick={() => navigate("/")}>
+          👑 <span>Lapking</span>Hub
+        </h2>
       </div>
 
       <div className="header-right">
-        <span className="icon">favorite_border</span>
-        <span className="icon">shopping_cart</span>
+        <MdFavoriteBorder size={24} />
+        <MdShoppingCart size={24} onClick={() => navigate("/cart")} />
       </div>
     </header>
   );
