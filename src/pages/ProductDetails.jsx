9a52c7590069e5b0ec3370c2748f78cee589a969
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-24 10:39:36 +0530

    Update ProductDetails.jsx

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import supabase from "../supabaseClient";
+import { supabase } from "../supabaseClient";
 
 export default function ProductDetails() {
   const { id } = useParams();
