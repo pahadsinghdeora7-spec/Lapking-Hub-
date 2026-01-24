72f45c51d1b962e6f4711aa14bb6b3314bdf94c9
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-24 08:10:44 +0530

    Update AdminCategories.jsx

--- src/admin/AdminCategories.jsx
+++ src/admin/AdminCategories.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "../supabaseClient.js";
+import { supabase } from "../supabaseClient.jsx";
 export default function AdminCategories() {
   const [categories, setCategories] = useState([]);
   const [name, setName] = useState("");
