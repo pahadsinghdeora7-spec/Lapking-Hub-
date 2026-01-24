4ed676c38d9c8dfb85631d3b3924a705ff96395b
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-24 17:06:16 +0530

    Update HomeSlider.jsx

--- src/components/HomeSlider.jsx
+++ src/components/HomeSlider.jsx
@@ -1,13 +1,33 @@
+import { useEffect, useState } from "react";
 import "./homeSlider.css";
 
+const banners = [
+  "/banners/1.jpg",
+  "/banners/2.jpg",
+  "/banners/3.jpg",
+];
+
 export default function HomeSlider() {
+  const [index, setIndex] = useState(0);
+
+  // 🔒 SLIDER SPEED LOCKED
+  const SLIDE_TIME = 4500; // 4.5 seconds (perfect speed)
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setIndex((prev) => (prev + 1) % banners.length);
+    }, SLIDE_TIME);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <div className="home-slider">
-      <div className="slider-track">
-        <img src="/banners/1.jpg" alt="Banner 1" />
-        <img src="/banners/2.jpg" alt="Banner 2" />
-        <img src="/banners/3.jpg" alt="Banner 3" />
-      </div>
+      <img
+        src={banners[index]}
+        alt="Lapking Hub Banner"
+        className="slider-image"
+      />
     </div>
   );
 }
