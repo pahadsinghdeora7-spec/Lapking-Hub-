c2899e9fbf85141d6a7487a88ea6c5b7905ea006
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-24 09:30:02 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../supabaseClient.js";
-import "./Home.css";
+import "./home.css";
 
 export default function Home() {
   const navigate = useNavigate();
