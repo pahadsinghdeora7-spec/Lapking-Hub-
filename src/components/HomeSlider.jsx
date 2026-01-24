ad04ea27cb29dbf3294ca53dd8eddb4452d2807b
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-24 16:58:11 +0530

    Create HomeSlider.jsx

---
+++ src/components/HomeSlider.jsx
@@ -0,0 +1,13 @@
+import "./homeSlider.css";
+
+export default function HomeSlider() {
+  return (
+    <div className="home-slider">
+      <div className="slider-track">
+        <img src="/banners/1.jpg" alt="Banner 1" />
+        <img src="/banners/2.jpg" alt="Banner 2" />
+        <img src="/banners/3.jpg" alt="Banner 3" />
+      </div>
+    </div>
+  );
+}
