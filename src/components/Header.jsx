68a904b0f7368a5b243dab84a527226955295eef
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-25 16:40:04 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import DrawerMenu from "./DrawerMenu";
+import SearchBar from "./SearchBar";
 
 export default function Header() {
   const [open, setOpen] = useState(false);
