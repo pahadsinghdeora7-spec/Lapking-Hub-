d61ff9e6733359d3f1605b3b44c10f928f76116f
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-25 09:13:39 +0530

    Update ProductCard.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -12,16 +12,20 @@ const ProductCard = ({ product }) => {
 
       <h3 className="product-name">{product.name}</h3>
 
-      {/* BRAND - CATEGORY - PART */}
+      {/* BRAND | CATEGORY | PART */}
       <div className="info-row">
+
+        {/* LEFT */}
         <span className="brand-text">
           Brand: {product.brand || "-"}
         </span>
 
+        {/* CENTER */}
         <span className="category-text">
           {product.category || ""}
         </span>
 
+        {/* RIGHT */}
         <div className="part-stock">
           <span className="part-text">
             Part No: {product.part_number || "-"}
@@ -35,6 +39,7 @@ const ProductCard = ({ product }) => {
             {product.stock > 0 ? "In Stock" : "Out of Stock"}
           </span>
         </div>
+
       </div>
 
       <div className="price">
