58f715024aa794aa1c119bf4542c9858e03fe808
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-25 08:57:50 +0530

    Update ProductDetails.jsx

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import supabase from "../supabaseClient";
+import { supabase } from "../supabaseClient";
 import "../pages/ProductDetails.css";
 
 const ProductDetails = () => {
