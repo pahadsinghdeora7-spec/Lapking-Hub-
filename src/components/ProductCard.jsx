9539487d3026b8cdad8379f1f3ed37b4b462871d
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-25 09:20:06 +0530

    Update ProductCard.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -15,32 +15,32 @@ const ProductCard = ({ product }) => {
       {/* BRAND | CATEGORY | PART */}
       <div className="info-row">
 
-        {/* LEFT */}
-        <span className="brand-text">
-          Brand: {product.brand || "-"}
-        </span>
+  {/* LEFT */}
+  <span className="brand-text">
+    Brand: {product.brand || "-"}
+  </span>
 
-        {/* CENTER */}
-        <span className="category-text">
-          {product.category || ""}
-        </span>
+  {/* CENTER CATEGORY */}
+  <span className="category-text">
+    {product.category || ""}
+  </span>
 
-        {/* RIGHT */}
-        <div className="part-stock">
-          <span className="part-text">
-            Part No: {product.part_number || "-"}
-          </span>
+  {/* RIGHT */}
+  <div className="part-stock">
+    <span className="part-text">
+      Part No: {product.part_number || "-"}
+    </span>
 
-          <span
-            className={
-              product.stock > 0 ? "stock-in" : "stock-out"
-            }
-          >
-            {product.stock > 0 ? "In Stock" : "Out of Stock"}
-          </span>
-        </div>
+    <span
+      className={
+        product.stock > 0 ? "stock-in" : "stock-out"
+      }
+    >
+      {product.stock > 0 ? "In Stock" : "Out of Stock"}
+    </span>
+  </div>
 
-      </div>
+</div>
 
       <div className="price">
         ₹{product.price || 0}
