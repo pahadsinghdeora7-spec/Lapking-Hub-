fb0fb995aa730b1995e18e97833854170d0838c7
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-27 12:24:11 +0530

    Update AddProductPage.jsx

--- src/admin/AddProductPage.jsx
+++ src/admin/AddProductPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { supabase } from "../supabaseClient";
-import "./adminAddProduct.css";
+import "./AddProductPage.css";
 
 export default function AdminAddProduct() {
 
