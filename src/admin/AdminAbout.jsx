dba762e0e06a1a356e5c15b0849854d2fab60339
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-28 08:45:24 +0530

    Update AdminAbout.jsx

--- src/admin/AdminAbout.jsx
+++ src/admin/AdminAbout.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../supabaseClient";
+import "./AboutUs.css";
 
 export default function AdminAbout() {
   const [form, setForm] = useState({
