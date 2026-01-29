fc39c7bbc47fc514f074c1defc32e483c40677a1
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-29 12:57:22 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../supabaseClient";
-import "./Auth.css";
+import "./Login.css";
 
 export default function Signup() {
   const navigate = useNavigate();
