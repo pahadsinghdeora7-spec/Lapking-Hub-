1ab67ad03a33a9f5d071d33f2677e28f65616b75
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-30 13:57:16 +0530

    Update AdminOrders.jsx

--- src/admin/AdminOrders.jsx
+++ src/admin/AdminOrders.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../supabaseClient";
-import "./adminOrders.css";
+import "./AdminOrders.css";
 
 export default function AdminOrders() {
   const [orders, setOrders] = useState([]);
