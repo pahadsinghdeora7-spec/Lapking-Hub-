eb0df4355e0131d8f35f35b6ed75225436772640
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-01-30 13:47:09 +0530

    Update AdminOrderView.jsx

--- src/admin/AdminOrderView.jsx
+++ src/admin/AdminOrderView.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { supabase } from "../supabaseClient";
+import "./AdminOrders.css";
 
 
 export default function AdminOrderView({ order, onClose, onSaved }) {
