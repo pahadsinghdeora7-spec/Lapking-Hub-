70438a8505c2c828b836bc244eeb73b81face8d6
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-02-01 20:47:05 +0530

    Update GlobalLoader.jsx

--- src/components/GlobalLoader.jsx
+++ src/components/GlobalLoader.jsx
@@ -1,6 +1,12 @@
+import { useLoader } from "../context/LoaderContext";
 import "./GlobalLoader.css";
 
 export default function GlobalLoader() {
+  const { loading } = useLoader();
+
+  // ❌ loading false → kuch bhi mat dikhao
+  if (!loading) return null;
+
   return (
     <div className="global-loader">
       <div className="loader-box">
