36b34c87d82b284032ec2afcf5e177245c37c19e
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-02-09 23:42:18 +0530

    Update xml.js

--- functions/sitemap/xml.js
+++ functions/sitemap/xml.js
@@ -1,33 +1,49 @@
-<?xml version="1.0" encoding="UTF-8"?>
+export async function onRequest(context) {
+  const SUPABASE_URL = context.env.SUPABASE_URL;
+  const SUPABASE_ANON_KEY = context.env.SUPABASE_ANON_KEY;
+
+  const res = await fetch(
+    `${SUPABASE_URL}/rest/v1/products?select=slug,updated_at`,
+    {
+      headers: {
+        apikey: SUPABASE_ANON_KEY,
+        Authorization: `Bearer ${SUPABASE_ANON_KEY}`,
+      },
+    }
+  );
+
+  const products = await res.json();
+
+  let urls = "";
+
+  for (const p of products) {
+    if (!p.slug) continue;
+
+    urls += `
+  <url>
+    <loc>https://lapkinghub.com/product/${p.slug}</loc>
+    <lastmod>${new Date(p.updated_at).toISOString().split("T")[0]}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`;
+  }
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-  <!-- Home -->
   <url>
     <loc>https://lapkinghub.com/</loc>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
   </url>
 
-  <!-- Products Listing -->
-  <url>
-    <loc>https://lapkinghub.com/products</loc>
-    <changefreq>daily</changefreq>
-    <priority>0.9</priority>
-  </url>
+  ${urls}
 
-  <!-- Product Pages -->
-  <url>
-    <loc>https://lapkinghub.com/product/hp-65w-laptop-charger</loc>
-    <lastmod>2026-02-09</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
+</urlset>`;
 
-  <url>
-    <loc>https://lapkinghub.com/product/dell-original-battery</loc>
-    <lastmod>2026-02-08</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-
-</urlset>
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml; charset=UTF-8",
+    },
+  });
+}
