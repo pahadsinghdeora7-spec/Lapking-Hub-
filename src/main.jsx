13deb4aabe5308a3ba4b28cb4323be25cd091aa6
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-02-10 15:01:18 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./Styles.css";
 
+import { LoaderProvider } from "./context/LoaderContext";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <LoaderProvider>
+      <App />
+    </LoaderProvider>
   </React.StrictMode>
 );
