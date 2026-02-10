aa6a817d8d4a50d05393c55d5ff7d1125c0f0ed4
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-02-10 11:13:21 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-// src/main.jsx
-throw new Error("Lapking Hub TEST ERROR");
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./Styles.css";
 
-import { LoaderProvider } from "./context/LoaderContext"; // ✅ ADD
+import { LoaderProvider } from "./context/LoaderContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LoaderProvider>      {/* ✅ WRAP APP */}
+    <LoaderProvider>
       <App />
     </LoaderProvider>
   </React.StrictMode>
