00268e674734fd43caa1eaa69021bdd0713faffc
pahadsinghdeora7-spec <pahadsinghdeora7@gmail.com>
2026-02-15 18:18:07 +0530

    Update sitemap.xml.js

--- api/sitemap.xml.js
+++ api/sitemap.xml.js
@@ -40,7 +40,7 @@ export default async function handler(req, res) {
       .join("");
 
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://lapkinghub.com/</loc>
     <changefreq>daily</changefreq>
